46918ed9952aef6267e8ea9adf8e50ed9e5b08fc
Nate Coraor <nate@bx.psu.edu>
2018-08-20 14:13:38 -0400

    Fetch virtualenv on macOS

--- .ci/wheels-install.sh
+++ .ci/wheels-install.sh
@@ -3,6 +3,8 @@ set -e
 set -xv
 
 starforge="git+https://github.com/natefoo/starforge.git@py3-wheels#egg=starforge"
+virtualenv_vers="16.0.0"
+virtualenv="https://files.pythonhosted.org/packages/33/bc/fa0b5347139cd9564f0d44ebd2b147ac97c36b2403943dbee8a25fd74012/virtualenv-16.0.0.tar.gz"
 
 case $TRAVIS_OS_NAME in
     osx)
@@ -23,10 +25,12 @@ case $TRAVIS_OS_NAME in
         sudo ln -s /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer /Developer
         curl -LO https://www.python.org/ftp/python/${PY}.${pypt}/python-${PY}.${pypt}-macosx10.6.pkg
         sudo installer -pkg python-${PY}.${pypt}-macosx10.6.pkg -target /
-        virtualenv -p /Library/Frameworks/Python.framework/Versions/${PY}/bin/python${PY%%.*} $HOME/venv
+        curl -O "$virtualenv"
+        tar zxvf virtualenv-${virtualenv_vers}.tar.gz
+        /Library/Frameworks/Python.framework/Versions/${PY}/bin/python${PY%%.*} ./virtualenv-${virtualenv_vers}/virtualenv.py $HOME/venv
         . $HOME/venv/bin/activate
         # pip can't upgrade itself due to TLS errors
-        curl https://bootstrap.pypa.io/get-pip.py | python
+        #curl https://bootstrap.pypa.io/get-pip.py | python
         pip install $starforge
         pip install delocate
         ;;
