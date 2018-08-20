855661a22820804874dafea9174c7c1cb8a292c9
Nate Coraor <nate@bx.psu.edu>
2018-08-20 14:53:37 -0400

    Fixes for purepy wheel

--- .ci/wheels-build.sh
+++ .ci/wheels-build.sh
@@ -4,20 +4,22 @@ set -xv
 
 . $HOME/venv/bin/activate
 
+case $TRAVIS_OS_NAME in
+    osx)
+        images=(ci/${TRAVIS_OS_NAME}-${PY})
+        ;;
+    linux)
+        images=(ci/${TRAVIS_OS_NAME}-${PY}:x86_64 ci/${TRAVIS_OS_NAME}-${PY}:i686)
+        ;;
+esac
+
 while read meta; do
     _f=${meta#wheels/}
     wheel=${_f%%/*}
-    echo "Building '$wheel' wheel from $meta"
-    case $TRAVIS_OS_NAME in
-        osx)
-            starforge --config-file="starforge.yml" --debug bdist_wheel --wheels-config="$meta" --image=ci/${TRAVIS_OS_NAME}-${PY} --fetch-srcs $wheel
-            starforge --config-file="starforge.yml" --debug test_wheel --wheels-config="$meta" --image=ci/${TRAVIS_OS_NAME}-${PY} $wheel
-            ;;
-        linux)
-            starforge --config-file="starforge.yml" --debug wheel --wheels-config="$meta" --image=ci/${TRAVIS_OS_NAME}-${PY}:x86_64 --no-sdist $wheel
-            starforge --config-file="starforge.yml" --debug wheel --wheels-config="$meta" --image=ci/${TRAVIS_OS_NAME}-${PY}:i686 --no-sdist $wheel
-            starforge --config-file="starforge.yml" --debug test_wheel --wheels-config="$meta" --image=ci/${TRAVIS_OS_NAME}-${PY}:x86_64 $wheel
-            starforge --config-file="starforge.yml" --debug test_wheel --wheels-config="$meta" --image=ci/${TRAVIS_OS_NAME}-${PY}:i686 $wheel
-            ;;
-    esac
+    for image in ${images[@]}; do
+        echo "Building '$wheel' wheel on image '$image' from $meta"
+        starforge --config-file="starforge.yml" --debug bdist_wheel --wheels-config="$meta" --image="$image" --fetch-srcs "$wheel"
+        echo "Testing '$wheel' wheel"
+        starforge --config-file="starforge.yml" --debug test_wheel --wheels-config="$meta" --image="$image" "$wheel"
+    done
 done < __wheels.txt
--- .ci/wheels-deploy.sh
+++ .ci/wheels-deploy.sh
@@ -2,6 +2,11 @@
 set -e
 #set -xv
 
+if [ "$(echo *.whl)" == '*.whl' ]; then
+    echo "deploy: no wheels built"
+    exit 0
+fi
+
 openssl aes-256-cbc -K $encrypted_632cb16dd578_key -iv $encrypted_632cb16dd578_iv -in .ci/starforge-depot.key.enc -out .ci/starforge-depot.key -d
 echo 'wheels.galaxyproject.org ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCXELLgds0rObMT25AWrUFBCx6W6Z5j/wTlT54ke/grCo6RaQ9gLc5GHrJnApKpqVzyNbCIdhz/50QpzVr6EsKSITfadkoDfmmgISq6i2R+OpVgjrBvNWrUtNy6qcZqvgReOyc7yZGlhhZFU8KMTGb2Qajo3TNYiSo9Sbt96HHQIAni1xcocI1Wqw6v/wKlg+2qQgO5g56XzVeZ4yS7zTlKgLexm1GIG3CNI42lndQJJ2pVD/TJ4CDC3CV2HRv5wpJ8Y/T5/7iZr0H/5lvVd5S8wxAx5xrJr1UOIQ/76fjymq5L0kK9FpsX3vjHAOvVkwykGm2I/P4YXB6I+nuJCYtP' >> $HOME/.ssh/known_hosts
 echo 'wheels.galaxyproject.org ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBPTnO0DR+VKfErJfN20IY6rjHbbismna9z6SQLEYzbQwpc20fQ06qmMsGJpLD4stBB2zQibTmKRd00QNWAfdILY=' >> $HOME/.ssh/known_hosts
