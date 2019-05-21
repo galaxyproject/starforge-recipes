92ca354778bd048e9a96b48f7edff5519c8924e3
Nate Coraor <nate@bx.psu.edu>
2019-05-21 13:50:29 -0400

    macOS build fixes

--- .ci/wheel-cext-builder-setup.sh
+++ .ci/wheel-cext-builder-setup.sh
@@ -27,7 +27,7 @@ case $TRAVIS_OS_NAME in
         virtualenv -p /Library/Frameworks/Python.framework/Versions/${PY}/bin/python${PY%%.*} $STARFORGE_VENV
         # $STARFORGE_VENV should still be activated
         #. $HOME/buildenv/bin/activate
-        pip install "$DELOCATE" "$STARFORGE"
+        pip install "$DELOCATE" "${STARFORGE}[lzma]"
         ;;
     linux)
         for arch in x86_64 i686; do
